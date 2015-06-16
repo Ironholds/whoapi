4117e8b39c6fd39bdf8d7f170438be363a16cff3
ironholds <ironholds@gmail.com>
2015-06-16 19:13:47 -0400

    finish tests

--- tests/testthat/test_info.R
+++ tests/testthat/test_info.R
@@ -12,3 +12,17 @@ test_that("Test we can identify if a domain is blacklisted", {
   expect_that(is.list(result), equals(TRUE))
   expect_that("blacklisted" %in% names(result), equals(TRUE))
 })
+
+test_that("Test we can run whois against a domain", {
+  token <- whoapi_token("demokey")
+  result <- whois_info(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(c("contacts","whois_server","nameservers") %in% names(result), equals(c(TRUE,TRUE,TRUE)))
+})
+
+test_that("Certificate information can be retrieved", {
+  token <- whoapi_token("demokey")
+  result <- certificate_info(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(c("status","certondomain") %in% names(result), equals(c(TRUE,TRUE)))
+})
---
+++ tests/testthat/test_metadata.R
@@ -0,0 +1,30 @@
+context("Test retrieval of metadata")
+
+test_that("Domain search ranking can be retrieved", {
+  token <- whoapi_token("demokey")
+  result <- domain_rank(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(names(result), equals(c("status","pr","alexa_reach","alexa_popularity","alexa_linksin", "email", "title")))
+})
+
+test_that("domain search results can be retrieved", {
+  token <- whoapi_token("demokey")
+  result <- domain_search(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(names(result), equals(c("status","google_results","bing_results")))
+})
+
+test_that("Metadata (in the HTML sense) can be retrieved", {
+  token <- whoapi_token("demokey")
+  result <- domain_metadata(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(names(result), equals(c("status","title","description")))
+
+})
+
+test_that("Geolocation info can be retrieved", {
+  token <- whoapi_token("demokey")
+  result <- domain_location(token, "whoapi.com")
+  expect_that(is.list(result), equals(TRUE))
+  expect_that(names(result), equals(c("status","ip","geo_cc","geo_country","geo_city", "geo_latitude", "geo_longitude")))
+})
